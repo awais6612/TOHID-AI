009b24de0d10a6328140dc88c2662c2a9b91a8bc
awais6612 <malihachuseambi@gmail.com>
2025-04-21 11:30:54 +0500

    Update config.js
    ./Tak

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'TOHID-AI';
 global.pairingNumber = process.env.BOT_NUMBER || '918930358452' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'VT0hwA5B#ocM9aOhZLXWqgAF4K6SYistTF_gMQnHfR812BpIqZIQ' ;  // put your session id here
 
 global.mods = []
 global.prems = []
